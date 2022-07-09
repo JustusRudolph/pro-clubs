011e1a86336047cbd7d0494e28cc7531dd0c3c01
Tim Lukas Unruh <lukas@unruhs.de>
2022-07-09 18:08:26 +0200

    Added directory for static data.

--- src/vision/value_checking.py
+++ src/vision/value_checking.py
@@ -1,49 +1,4 @@
-# dictionary that defines the range for game data
-game_range_dict = {
-    "Possession": [10, 90],
-    "Shots": [0, 30],
-    "ExpectedGoals": [0, 20],
-    "Passes": [0, 500],
-    "Tackles": [0, 100],
-    "TacklesWon": [0, 100],
-    "Interceptions": [0, 100],
-    "Saves": [0, 100],
-    "FoulsCommitted": [0, 100],
-    "Offsides": [0, 100],
-    "Corners": [0, 100],
-    "FreeKicks": [0, 100],
-    "PenaltyKicks": [0, 10],
-    "YellowCards": [0, 11],
-    "RedCards": [0, 6],
-    "DribbleSuccessRate": [0, 100],
-    "ShotAccuracy": [0, 100],
-    "PassAccuracy": [0, 100]
-}
-
-# dictionary that defines the range for player data
-player_range_dict = {
-    "Rating": [0, 10],
-    "Goals": [0, 10],
-    "Assists": [0, 10],
-    "Shots": [0, 20],
-    "ShotAccuracy": [0, 100],
-    "Passes": [0, 100],
-    "PassAccuracy": [0, 100],
-    "Dribbles": [0, 100],
-    "DribbleSuccessRate": [0, 100],
-    "Tackles": [0, 100],
-    "TackleSuccessRate": [0, 100],
-    "Offsides": [0, 100],
-    "FoulsCommitted": [0, 100],
-    "PossessionWon": [0, 100],
-    "PossessionLost": [0, 100],
-    "MinutesPlayed": [0, 100],
-    "DistanceCovered": [0, 100],
-    "DistanceSprinted": [0, 100],
-    "YellowCard": [0, 1],
-    "RedCard": [0, 1]
-}
-
+from static_data import valid_ranges
 
 def check_game_values(input_dict):
     """
@@ -54,8 +9,8 @@ def check_game_values(input_dict):
     Parameters:
         input_dict(dict): the dictionary to check
     """
-    for key in game_range_dict:
-        pos_range = game_range_dict[key]
+    for key in valid_ranges.game_exp_range_dict:
+        pos_range = valid_ranges.game_exp_range_dict[key]
         value = input_dict[key]
 
         for i in range(2):
@@ -72,8 +27,8 @@ def check_player_values(input_dict):
     Parameters:
         input_dict(dict): the dictionary to check
     """
-    for key in player_range_dict:
-        pos_range = player_range_dict[key]
+    for key in valid_ranges.player_exp_range_dict:
+        pos_range = valid_ranges.player_exp_range_dict[key]
         value = input_dict[key]
         if not (pos_range[0] <= value <= pos_range[1]):
             input_dict[key] = -1
